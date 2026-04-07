e7994b6518cf7913396fcce4fa30d97d5d27eeaf
kavya <kavya.actimize@gmail.com>
2026-04-07 12:53:48 +0530

    src folder

--- src/Colors.js
+++ src/Colors.js
@@ -7,7 +7,7 @@ const Colors={
     red:"#f90303e3",
     violet:"#54044d",
     white: localStorage.getItem("darkMode") !== "true" ? "#000604": "#f6f3f3",
-    black:localStorage.getItem("darkMode") == "true" ? "#000604": "#f6f3f3",
+    black:localStorage.getItem("darkMode") === "true" ? "#000604": "#f6f3f3",
    
     orange:'#f56702ef'
 }
