41656f03de6bb83730c58eb76858916a7f2d0821
kavya <kavya.actimize@gmail.com>
2026-04-07 12:14:26 +0530

    src folder

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -9,7 +9,7 @@ export default function AppRouter({ darkMode, toggleDarkMode }) {
   return (
     <Routes>
       <Route
-        path="/admin"
+        path=""
         element={
           <AdminDashboard
             darkMode={darkMode}
